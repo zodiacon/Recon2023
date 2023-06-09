4df2b9693467248f0b581a49b5434e3c0665d8a3
Pavel Yosifovich <zodiacon@live.com>
2023-06-09 15:15:24 -0400

    Added ListModules implementation

--- ListModules/ListModules.cpp
+++ ListModules/ListModules.cpp
@@ -3,16 +3,36 @@
 
 #include <Windows.h>
 #include <stdio.h>
+#include <Psapi.h>
 #include "..\ProcAccess\ProcAccessPublic.h"
 
+bool ListModules(HANDLE hProcess) {
+	HMODULE hModule[1024];
+	DWORD needed;
+	if (!EnumProcessModules(hProcess, hModule, sizeof(hModule), &needed))
+		return false;
+
+	WCHAR name[256];
+	for (ULONG i = 0; i < needed / sizeof(HMODULE); i++) {
+		::GetModuleBaseName(hProcess, hModule[i], name, _countof(name));
+		printf("0x%p: %ws\n", hModule[i], name);
+	}
+	return true;
+}
+
 int main(int argc, const char* argv[]) {
+	if (argc < 2) {
+		printf("Usage: ListModules <pid>\n");
+		return 0;
+	}
 	int pid = atoi(argv[1]);
 	auto hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
 	if (hProcess) {
 		printf("Success in OpenProcess!\n");
+		ListModules(hProcess);
 		return 0;
 	}
-	printf("OpenProces failed!\n");
+	printf("OpenProcess failed!\n");
 
 	HANDLE hDevice = CreateFile(L"\\\\.\\ProcAccess",
 		GENERIC_WRITE | GENERIC_READ, 0, nullptr,
@@ -27,6 +47,7 @@ int main(int argc, const char* argv[]) {
 		&pid, sizeof(pid), &hProcess, sizeof(hProcess),
 		&ret, nullptr)) {
 		printf("Success!\n");
+		ListModules(hProcess);
 	}
 	else {
 		printf("Error (%u)\n", GetLastError());
